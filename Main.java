97df5f02624d577036626cce1962d4d79d332b89
erickguerra2 <eagi578@gmail.com>
2024-03-19 23:01:41 -0600

    Mostrar asociaciones

--- Main.java
+++ Main.java
@@ -8,8 +8,12 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
+    
     
     /** 
      * @param args
@@ -17,20 +21,50 @@ public class Main {
     public static void main(String[] args) {
         BinaryTree<Association<String, String>> dictionary = new BinaryTree<>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader("diccionario.txt"))) {
+        List<Association<String, String>> associations = loadDictionary("diccionario.txt");
+        
+        for (Association<String, String> association : associations) {
+            dictionary.insert(association);
+        }
+
+        System.out.println("Arbol ordenado por la palabra en inglés:");
+        for (Association<String, String> association : associations) {
+            System.out.println("(" + association.getKey() + ", " + association.getValue() + ")");
+        }
+
+        System.out.println("\nOración:");
+        translateSentence("texto.txt", dictionary);
+    }
+
+    
+    /** 
+     * @param filename
+     * @return List<Association<String, String>>
+     */
+    private static List<Association<String, String>> loadDictionary(String filename) {
+        List<Association<String, String>> associations = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] parts = line.split(",");
                 String wordEnglish = parts[0].trim().substring(1);
                 String wordSpanish = parts[1].trim().substring(0, parts[1].trim().length() - 1);
                 Association<String, String> association = new Association<>(wordEnglish, wordSpanish);
-                dictionary.insert(association);
+                associations.add(association);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        Collections.sort(associations, (a1, a2) -> a1.getKey().compareTo(a2.getKey()));
+        return associations;
+    }
 
-        try (BufferedReader br = new BufferedReader(new FileReader("texto.txt"))) {
+    
+    /** 
+     * @param dictionary
+     */
+    private static void translateSentence(String filename, BinaryTree<Association<String, String>> dictionary) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] words = line.split("\\s+");
